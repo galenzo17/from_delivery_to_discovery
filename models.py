48221374d16b913b92aff32b53b5db1faeec2e0f
Agustin Bereciartua Castillo <galenzo17@users.noreply.github.com>
2024-10-14 21:01:20 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import Optional, List
+from typing import List, Optional
 
 class User(BaseModel):
     username: str
@@ -8,9 +8,9 @@ class User(BaseModel):
 class Ticket(BaseModel):
     id: str
     title: str
-    description: Optional[str]
-    acceptance_criteria: Optional[List[str]]
+    description: Optional[str] = None
+    acceptance_criteria: Optional[List[str]] = None
 
 class TicketUpdateRequest(BaseModel):
-    description: Optional[str]
-    acceptance_criteria: Optional[List[str]]
+    description: Optional[str] = None
+    acceptance_criteria: Optional[List[str]] = None
