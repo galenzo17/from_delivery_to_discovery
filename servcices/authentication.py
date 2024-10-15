1f7c80c64a4a03705fe90f8dadef0b08a602c8f0
Agustin Bereciartua Castillo <galenzo17@users.noreply.github.com>
2024-10-14 21:01:00 -0300

    Update authentication.py

--- servcices/authentication.py
+++ servcices/authentication.py
@@ -1,9 +1,8 @@
 from fastapi import Depends, HTTPException
 from models import User
 
-def get_current_user(token: str = "dummy_token") -> User:
-    # Aquí implementarías la lógica real de autenticación
-    if token == "dummy_token":
+def get_current_user(token: str) -> User:
+    if token == "valid_token":
         return User(username="test_user", token=token)
     else:
         raise HTTPException(status_code=401, detail="Token inválido")
